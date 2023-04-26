bb657865eef61904b54f96c268c31d50c4789e8d
zoheirkabuli <zoheyrk@gmail.com>
2023-04-26 19:04:30 +0330

    first commit

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\Auth\SocialiteController;
+use App\Http\Controllers\DashboardController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [DashboardController::class, 'index'])->middleware(['auth', 'verified']);
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/error', function () {
+    abort(500);
+});
+
+Route::get('/auth/redirect/{provider}', [SocialiteController::class, 'redirect']);
+
+require __DIR__.'/auth.php';
